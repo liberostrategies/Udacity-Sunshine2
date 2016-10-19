e682ab76085001b7a49c879eb0683172426609ec
liberostrategies <liberostrategies@gmail.com>
2016-10-19 11:24:31 -0400

    Implement the Details screen.

---
+++ app/src/main/java/com/example/android/sunshine/app/DetailFragment.java
@@ -0,0 +1,195 @@
+/*
+ * Copyright (c) 2016. Libero Strategies, LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and Confidential
+ */
+
+package com.example.android.sunshine.app;
+
+/**
+ * Created by pink on 10/19/2016.
+ */
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.android.sunshine.app.data.WeatherContract;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class DetailFragment extends Fragment
+        implements LoaderManager.LoaderCallbacks<Cursor> {
+    private final String LOG_TAG = DetailFragment.class.getSimpleName();
+
+    private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+
+    private String mForecastStr;
+
+    private ShareActionProvider mShareActionProvider;
+
+    private static final int DETAIL_LOADER = 0;
+
+    private static final String[] DETAILED_COLUMNS = {
+            WeatherContract.WeatherEntry.TABLE_NAME + "." + WeatherContract.WeatherEntry._ID,
+            WeatherContract.WeatherEntry.COLUMN_DATE,
+            WeatherContract.WeatherEntry.COLUMN_SHORT_DESC,
+            WeatherContract.WeatherEntry.COLUMN_MAX_TEMP,
+            WeatherContract.WeatherEntry.COLUMN_MIN_TEMP,
+            WeatherContract.WeatherEntry.COLUMN_HUMIDITY,
+            WeatherContract.WeatherEntry.COLUMN_PRESSURE,
+            WeatherContract.WeatherEntry.COLUMN_WIND_SPEED,
+            WeatherContract.WeatherEntry.COLUMN_DEGREES,
+    };
+
+    private static final int COL_WEATHER_ID = 0;
+    private static final int COL_WEATHER_DATE = 1;
+    private static final int COL_WEATHER_DESC = 2;
+    private static final int COL_WEATHER_MAX_TEMP = 3;
+    private static final int COL_WEATHER_MIN_TEMP = 4;
+    private static final int COL_WEATHER_HUMIDITY = 5;
+    private static final int COL_WEATHER_PRESSURE = 6;
+    private static final int COL_WEATHER_WIND_SPEED = 7;
+    private static final int COL_WEATHER_DEGREES = 8;
+
+    private TextView mDateView;
+    private TextView mHighTempView;
+    private TextView mLowTempView;
+    private ImageView mImageView;
+    private TextView mForecastView;
+    private TextView mHumidityView;
+    private TextView mWindView;
+    private TextView mPressureView;
+
+    public DetailFragment() {
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+        TextView txtDetailedForecast = (TextView)rootView.findViewById(R.id.detailed_text);
+        Intent intent = getActivity().getIntent();
+        if (intent != null) {
+            mForecastStr = intent.getDataString();
+//            txtDetailedForecast.setText(mForecastStr);
+            mDateView = (TextView)rootView.findViewById(R.id.detailed_date);
+            mHighTempView = (TextView)rootView.findViewById(R.id.detailed_temp_max);
+            mLowTempView = (TextView)rootView.findViewById(R.id.detailed_temp_min);
+            mImageView = (ImageView)rootView.findViewById(R.id.detailed_image);
+            mForecastView = (TextView)rootView.findViewById(R.id.detailed_text);
+            mHumidityView = (TextView)rootView.findViewById(R.id.detailed_humidity);
+            mWindView = (TextView)rootView.findViewById(R.id.detailed_wind);
+            mPressureView = (TextView)rootView.findViewById(R.id.detailed_pressure);
+        }
+        return rootView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.detailfragment, menu);
+
+        // Retrieve the share menu item.
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+
+        // Get the provider and hold onto it to set/change the share intent.
+        mShareActionProvider =
+                (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+
+        // Attach intent to this ShareActionProvider. Update any time.
+        if (mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(createShareForecastIntent());
+        } else {
+            Log.d(LOG_TAG, "Share Action Provider is null");
+        }
+    }
+
+    private Intent createShareForecastIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        // Prevents activity sharing to from being added to activity stack.
+        // Returns to this app instead.
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT,
+                mForecastStr + FORECAST_SHARE_HASHTAG);
+        return shareIntent;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        getLoaderManager().initLoader(DETAIL_LOADER, null, this);
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        Log.v(LOG_TAG, "In onCreateLoader");
+        Intent intent = getActivity().getIntent();
+        if (intent == null) {
+            return null;
+        }
+
+        return new CursorLoader(
+                getActivity(),
+                intent.getData(),
+                DETAILED_COLUMNS,
+                null,
+                null,
+                null
+        );
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        Log.v(LOG_TAG, "In onLoadFinished");
+        if (!data.moveToFirst()) {
+            return;
+        }
+
+        mImageView.setImageResource(R.drawable.ic_launcher);
+        String dateText = Utility.getFriendlyDayString(getActivity(), data.getLong(COL_WEATHER_DATE));
+        mDateView.setText(dateText);
+        String weatherDescription = data.getString(COL_WEATHER_DESC);
+        mForecastView.setText(weatherDescription);
+        boolean isMetric = Utility.isMetric(getActivity());
+        String high = Utility.formatTemperature(getActivity(), data.getDouble(COL_WEATHER_MAX_TEMP), isMetric);
+        mHighTempView.setText(high);
+        String min = Utility.formatTemperature(getActivity(), data.getDouble(COL_WEATHER_MIN_TEMP), isMetric);
+        mLowTempView.setText(min);
+//        TextView detailTextView = (TextView) getView().findViewById(R.id.detailed_text);
+        mForecastStr = String.format("%s - %s - %s/%s", dateText, weatherDescription, high, min);
+        Log.d(LOG_TAG, "forecast string: " + mForecastStr);
+//        detailTextView.setText(mForecastStr);
+        mHumidityView.setText(getActivity().getString(R.string.format_humidity, data.getFloat(COL_WEATHER_HUMIDITY)));
+        mWindView.setText(Utility.getFormattedWind(getActivity(), data.getFloat(COL_WEATHER_WIND_SPEED), data.getFloat(COL_WEATHER_DEGREES)));
+        mPressureView.setText(getActivity().getString(R.string.format_pressure, data.getFloat(COL_WEATHER_PRESSURE)));
+
+        if (mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(createShareForecastIntent());
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
+}
