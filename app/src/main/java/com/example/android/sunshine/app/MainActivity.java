48239ff930f07fd5c63640b19ae028f22d7d0544
liberostrategies <liberostrategies@gmail.com>
2016-10-31 15:22:13 -0400

    Finish the SyncAdapter. Externalize my dev api key.

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -10,6 +10,8 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.android.sunshine.app.sync.SunshineSyncAdapter;
+
 
 public class MainActivity extends ActionBarActivity
     implements ForecastFragment.Callback {
@@ -88,6 +90,8 @@ public class MainActivity extends ActionBarActivity
         ForecastFragment forecastFragment = ((ForecastFragment)getSupportFragmentManager()
                 .findFragmentById(R.id.fragment_forecast));
         forecastFragment.setUseTodayLayout(!mTwoPane);
+
+        SunshineSyncAdapter.initializeSyncAdapter(this);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
