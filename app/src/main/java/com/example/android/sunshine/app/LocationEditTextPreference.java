b958bedfce64e78ec30d75f644ad4cbdec1ef0d0
liberostrategies <liberostrategies@gmail.com>
2016-11-14 01:50:19 -0500

    Implement limits in settings.

---
+++ app/src/main/java/com/example/android/sunshine/app/LocationEditTextPreference.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2016. Libero Strategies, LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and Confidential
+ */
+
+package com.example.android.sunshine.app;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.preference.EditTextPreference;
+import android.util.AttributeSet;
+
+/**
+ * Created by pink on 11/14/2016.
+ */
+
+public class LocationEditTextPreference extends EditTextPreference {
+    private static final int DEFAULT_MINIMUM_LOCATION_LENGTH = 2;
+    private int mMinLength;
+
+    public LocationEditTextPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        TypedArray a = context.getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.LocationEditTextPreference,
+                0, 0);
+        try {
+            mMinLength = a.getInteger(R.styleable.LocationEditTextPreference_minLength,
+                    DEFAULT_MINIMUM_LOCATION_LENGTH);
+        } finally {
+            a.recycle();
+        }
+    }
+}
