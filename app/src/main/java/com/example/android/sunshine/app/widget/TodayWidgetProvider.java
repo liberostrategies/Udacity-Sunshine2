19b7112665323f0108014f3df99bf95a2e9359d2
liberostrategies <liberostrategies@gmail.com>
2016-11-29 15:25:53 -0500

    Show icon and static text in today widget.

--- app/src/main/java/com/example/android/sunshine/app/widget/TodayWidgetProvider.java
+++ app/src/main/java/com/example/android/sunshine/app/widget/TodayWidgetProvider.java
@@ -15,6 +15,7 @@ import android.widget.RemoteViews;
 
 import com.example.android.sunshine.app.MainActivity;
 import com.example.android.sunshine.app.R;
+import com.example.android.sunshine.app.Utility;
 
 /**
  * Created by pink on 11/29/2016.
@@ -23,10 +24,18 @@ import com.example.android.sunshine.app.R;
 public class TodayWidgetProvider extends AppWidgetProvider {
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        int weatherArtResourceId = R.drawable.art_clear;
+        String description = "Clear";
+        double maxTemp = 24;
+        String formattedMaxTemperature = Utility.formatTemperature(context, maxTemp);
+
         for (int appWidgetId : appWidgetIds) {
             RemoteViews views = new RemoteViews(
                 context.getPackageName(),
                     R.layout.widget_today_small);
+            views.setImageViewResource(R.id.widget_icon, weatherArtResourceId);
+            views.setTextViewText(R.id.widget_high_temperature, formattedMaxTemperature);
+
             Intent intent = new Intent(context, MainActivity.class);
             PendingIntent pendingIntent = PendingIntent
                     .getActivity(context, 0, intent, 0);
